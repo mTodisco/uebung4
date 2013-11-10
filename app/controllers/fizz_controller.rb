4fe5bd48a5d720f49706bbfa50a57258d02d4d75
mTodisco <mtodisco@hwr-berlin.de>
2013-11-10 23:32:15 +0100

    Tabelle mit x

---
+++ app/controllers/fizz_controller.rb
@@ -0,0 +1,12 @@
+class FizzController < ApplicationController
+	def initialize
+	@fizz_name="Fizz"
+  @buzz_name="Buzz"
+  @fizz_number=3
+  @buzz_number=5
+  @max_number=100
+	end
+
+  def buzz
+  end
+end
