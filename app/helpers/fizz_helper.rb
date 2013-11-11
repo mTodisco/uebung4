350e5691f854b5e8cfe4988d90f5191f9fee8406
mTodisco <mtodisco@hwr-berlin.de>
2013-11-11 14:05:25 +0100

    14 Fertig

--- app/helpers/fizz_helper.rb
+++ app/helpers/fizz_helper.rb
@@ -1,2 +1,22 @@
 module FizzHelper
+  
+    def fizzy(num)
+    
+     if num%@fizz_number == 0 && num%@buzz_number == 0
+        return @fizz_name + @buzz_name
+      elsif num%@fizz_number == 0
+       return @fizz_name
+      elsif num%@buzz_number == 0
+        return @buzz_name
+      else
+       return num
+      end
+    
+     end
+  
+    def color_chooser(num)
+    
+      (num%@fizz_number == 0 && num%@buzz_number==0) ? "bgcolor=#fee000" : ""
+    end
+  
 end
